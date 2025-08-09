7a25a789a27c5dbe00b0aedf3f8725718bbec771
Dspofu <gabriel.pereira.goes01@gmail.com>
2025-08-08 21:51:19 -0300

    Adicionando sistema provisorio de segurança de temperatura e iniciando preparações para suporte multi-gpu

--- src/modules/coldGPU.py
+++ src/modules/coldGPU.py
@@ -18,19 +18,19 @@ def safe_temp(pipe, gpu: int = 0):
     if temp >= 90:
       pipe.enable_sequential_cpu_offload()
       alert(f"Acerto crítico, GPU marcou '{temp}°C', a recomendação é não realizar mais o uso da placa para estes fins ou ser feita um limpeza e troca dos condutores térmicos para a carcaça.")
-      time.sleep(3)
+      time.sleep(1)
     elif temp >= 85:
       pipe.enable_sequential_cpu_offload()
-      time.sleep(2)
+      time.sleep(0.7)
     elif temp >= 75:
       pipe.enable_model_cpu_offload()
-      time.sleep(1)
+      time.sleep(0.5)
     elif temp >= 70:
       pipe.enable_model_cpu_offload()
       if not critical_temp:
         alert(f"Sua GPU atingiu o limite para iniciar o processo de resfiamento.\nAtualmente com '{temp}°C', deste modo, o processo sofrera pausas continuas até a redução de temperatura ser feita.")
         critical_temp = True
-      time.sleep(0.5)
+      time.sleep(0.2)
     else:
       pipe.enable_model_cpu_offload()
   except pynvml.NVMLError as error:
