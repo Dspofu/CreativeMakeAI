02a89111638e59277844eada254f1a9e70bdf341
Dspofu <gabriel.pereira.goes01@gmail.com>
2025-08-13 13:06:19 -0300

    Logica de encerrar o processo de instalação após descomprimento de requisitos minimos

--- main.cpp
+++ main.cpp
@@ -40,44 +40,48 @@ int main() {
   if (d3d) {
     D3DADAPTER_IDENTIFIER9 adapterIdentifier;
     if (SUCCEEDED(d3d->GetAdapterIdentifier(D3DADAPTER_DEFAULT, 0, &adapterIdentifier))) {
-      if (adapterIdentifier.VendorId == 0x10DE) {
+      if (adapterIdentifier.VendorId != 0x10DE) {
         if (GlobalMemoryStatusEx(&memInfo)) {
           int memory = std::round(memInfo.ullTotalPhys / (1024.0f * 1024.0f * 1024.0f));
           if (memory <= 8) {
-            MessageBoxW(NULL, L"Nível de RAM muito baixo.\nPor motivo de segurança não será continuado o processo.", L"Pouca RAM no sistema", MB_ICONERROR);
-            std::cout << "O recomendado seria ter 32GB de RAM e foram identificados somente: " << memory << "GB de RAM" << std::endl;
-          }
-          char quest;
-          if (loopQuest(quest)) {
-            if (quest == '1' || quest == '2') {
-              if (quest == '2') system("winget install --id Python.Python.3.13 --version 3.13.6");
-              char option;
-              if (loopOption(option, adapterIdentifier, memory)) {
-                if (option == '1') {
-                  std::cout << "\nINICIANDO\n" << std::endl;
-                  system(".\\venv\\Scripts\\python.exe -m main");
-                  main();
-                } else if (option == '2') {
-                  system("python -m venv venv");
-                  system(".\\venv\\Scripts\\python.exe -m pip install --upgrade pip");
-                  system(".\\venv\\Scripts\\python.exe -m pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128");
-                  system(".\\venv\\Scripts\\python.exe -m pip install -r requirements.txt");
-                  system(".\\venv\\Scripts\\python.exe -m pip list");
-                  std::cout << "\nInicialize o venv para testar." << std::endl;
+            std::wstringstream content;
+            content << L"Processo encerrado por falta de memória RAM\nForam encontrados somente: " << memory << "GB de RAM.";
+            MessageBoxW(NULL, content.str().c_str(), L"Pouca RAM no sistema", MB_ICONERROR);
+            std::cout << "O recomendado seria ter 32GB de RAM e foram identificados somente: " << memory << "GB de RAM." << std::endl;
+            return 1;
+          } else {
+            char quest;
+            if (loopQuest(quest)) {
+              if (quest == '1' || quest == '2') {
+                if (quest == '2') system("winget install --id Python.Python.3.13 --version 3.13.6");
+                char option;
+                if (loopOption(option, adapterIdentifier, memory)) {
+                  if (option == '1') {
+                    std::cout << "\nINICIANDO\n" << std::endl;
+                    system(".\\venv\\Scripts\\python.exe -m main");
+                    main();
+                  } else if (option == '2') {
+                    system("python -m venv venv");
+                    system(".\\venv\\Scripts\\python.exe -m pip install --upgrade pip");
+                    system(".\\venv\\Scripts\\python.exe -m pip install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128");
+                    system(".\\venv\\Scripts\\python.exe -m pip install -r requirements.txt");
+                    system(".\\venv\\Scripts\\python.exe -m pip list");
+                    std::cout << "\nInicialize o venv para testar." << std::endl;
+                  }
                 }
               }
             }
           }
-        } else {
-          MessageBoxW(NULL, L"Sua Placa de video não é suportada.\nPor motivo de compatibilidade somente 'nvidia' é suportada.", L"Falha de incompatibilidade", MB_ICONERROR);
-          std::cout << "Por motivo de compatibilidade somente 'nvidia' é suportada." << std::endl;
-          return 0;
         }
+      } else {
+        MessageBoxW(NULL, L"Sua Placa de video não é suportada.\nPor motivo de compatibilidade somente 'nvidia' é suportada.", L"Falha de incompatibilidade", MB_ICONERROR);
+        std::cout << "Por motivo de compatibilidade somente 'nvidia' é suportada." << std::endl;
+        return 1;
       }
-    }
-    d3d->Release();
-  } else return 1;
-  return 0;
+      d3d->Release();
+    } else return 1;
+    return 0;
+  }
 }
 #else
 int main() {
